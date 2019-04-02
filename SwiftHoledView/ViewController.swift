80938444ac23ba4f7749ab007e1536c4358a5fc0
Mazy <mazy_ios@163.com>
2019-04-02 11:33:18 +0800

    功能初步实现,待细节优化

--- SwiftHoledView/ViewController.swift
+++ SwiftHoledView/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var holedView: SwiftHoledView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        holedView = SwiftHoledView(frame: view.bounds)
+        view.addSubview(holedView)
+        holedView.holeViewDelegate = self
+        holedView.addHoleCircleCenteredOnPosition(CGPoint(x: 25, y: 40), diameter: 40)
+        holedView.addHoleRectOnRect(CGRect(x: 10, y: 150, width: 300, height: 30))
+//        holedView.addRoundedRectHole(onRect: <#T##CGRect#>, cornerRadius: <#T##CGFloat#>, text: <#T##String#>, onPostion: <#T##HolePosition#>, margin: <#T##CGFloat#>)
+        
     }
 
 
+}
+
+extension ViewController: HoledViewDelegate {
+    func holedView(_ holedView: SwiftHoledView, didSelectHoleAtIndex index: Int) {
+        print(index)
+    }
+    
+    func holedView(_ holedView: SwiftHoledView, willAddLabel laber: UILabel, atIndex index: Int) {
+        
+    }
 }
 
